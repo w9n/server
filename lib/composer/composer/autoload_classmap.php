8385ebac7af78ea16166a7cd5b146c9128fc1d8a
Carl Schwan <carl@carlschwan.eu>
2022-10-16 19:57:31 +0200

    Split new method in a new group backend interface
    Better for backward compatibility, also move new interfaces to nc 26
    
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- lib/composer/composer/autoload_classmap.php
+++ lib/composer/composer/autoload_classmap.php
@@ -399,6 +399,7 @@ return array(
     'OCP\\Group\\Backend\\IIsAdminBackend' => $baseDir . '/lib/public/Group/Backend/IIsAdminBackend.php',
     'OCP\\Group\\Backend\\INamedBackend' => $baseDir . '/lib/public/Group/Backend/INamedBackend.php',
     'OCP\\Group\\Backend\\IRemoveFromGroupBackend' => $baseDir . '/lib/public/Group/Backend/IRemoveFromGroupBackend.php',
+    'OCP\\Group\\Backend\\ISearchableGroupBackend' => $baseDir . '/lib/public/Group/Backend/ISearchableGroupBackend.php',
     'OCP\\Group\\Backend\\ISetDisplayNameBackend' => $baseDir . '/lib/public/Group/Backend/ISetDisplayNameBackend.php',
     'OCP\\Group\\Events\\BeforeGroupCreatedEvent' => $baseDir . '/lib/public/Group/Events/BeforeGroupCreatedEvent.php',
     'OCP\\Group\\Events\\BeforeGroupDeletedEvent' => $baseDir . '/lib/public/Group/Events/BeforeGroupDeletedEvent.php',
