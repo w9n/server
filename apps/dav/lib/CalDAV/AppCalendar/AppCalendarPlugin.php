05eb849dc72be91056b4fa13429cfeeae9856b79
Ferdinand Thiessen <rpm@fthiessen.de>
2022-11-12 17:42:12 +0100

    Feature: Provide access to app generated calendars through CalDAV
    This adds CalDAV support for app generated calendars,
    which are registered to the nextcloud core.
    This is done by adding a dav plugin which wraps
    all ICalendarProviders into a Sabre plugin (inspired by the deck app).
    
    Add unit test for AppCalendar wrapper plugin and calendar object implementation.
    
    Signed-off-by: Ferdinand Thiessen <rpm@fthiessen.de>

---
+++ apps/dav/lib/CalDAV/AppCalendar/AppCalendarPlugin.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace OCA\DAV\CalDAV\AppCalendar;
+
+use OCA\DAV\CalDAV\Integration\ExternalCalendar;
+use OCA\DAV\CalDAV\Integration\ICalendarProvider;
+use OCP\Calendar\IManager;
+use Psr\Log\LoggerInterface;
+
+/* Plugin for wrapping application generated calendars registered in nextcloud core (OCP\Calendar\ICalendarProvider) */
+class AppCalendarPlugin implements ICalendarProvider {
+	protected IManager $manager;
+	protected LoggerInterface $logger;
+
+	public function __construct(IManager $manager, LoggerInterface $logger) {
+		$this->manager = $manager;
+		$this->logger = $logger;
+	}
+
+	public function getAppID(): string {
+		return 'dav-wrapper';
+	}
+
+	public function fetchAllForCalendarHome(string $principalUri): array {
+		return array_map(function ($calendar) use (&$principalUri) {
+			return new AppCalendar($this->getAppID(), $calendar, $principalUri);
+		}, $this->getWrappedCalendars($principalUri));
+	}
+
+	public function hasCalendarInCalendarHome(string $principalUri, string $calendarUri): bool {
+		return count($this->getWrappedCalendars($principalUri, [ $calendarUri ])) > 0;
+	}
+
+	public function getCalendarInCalendarHome(string $principalUri, string $calendarUri): ?ExternalCalendar {
+		$calendars = $this->getWrappedCalendars($principalUri, [ $calendarUri ]);
+		if (count($calendars) > 0) {
+			return new AppCalendar($this->getAppID(), $calendars[0], $principalUri);
+		}
+
+		return null;
+	}
+
+	protected function getWrappedCalendars(string $principalUri, array $calendarUris = []): array {
+		return array_values(
+			array_filter($this->manager->getCalendarsForPrincipal($principalUri, $calendarUris), function ($c) {
+				// We must not provide a wrapper for DAV calendars
+				return ! ($c instanceof \OCA\DAV\CalDAV\CalendarImpl);
+			})
+		);
+	}
+}
